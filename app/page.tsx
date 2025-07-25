a78ccc1dc6e8e015f007589e2b06208b225f60ac
Aaidt <aadits2004@gmail.com>
2025-07-25 23:10:43 +0530

    added header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,39 @@
-import Image from "next/image"
-import VideoBackground from "@/components/VideoBackground"
+"use client"
 
+import { motion } from "motion/react"
 
+const text = "Wallet_ops"
 export default function Home() {
-  return (
-    <div className="">
-      <VideoBackground />
-    </div>
+    const letterVariants = {
+        hidden: { opacity: 0, y: -8 },
+        visible: { opacity: 1, y: 0 },
 
+    }
+
+    const containerVariant = {
+        hidden: { opacity: 0 },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.1,
+                delayChildren: 0.3
+            }
+        }
+    }
+
+    return (
+        <div className="">
+            <motion.div variants={containerVariant} initial="hidden" animate="visible" 
+                className="text-5xl font-bold flex justify-center items-center pt-25">
+
+                {text.split("").map((char, idx) => (
+                    <motion.span 
+                        variants={letterVariants}
+                        key={idx}>
+                        {char}
+                    </motion.span>
+                ))}
+            </motion.div>
+        </div>
   )
 }
--- components/VideoBackground.tsx
+++ components/VideoBackground.tsx
@@ -8,9 +8,9 @@ const VideoBackground = () => {
           loop
           muted
           playsInline
-          className="object-cover w-full opacity-5"
+          className="object-cover w-full opacity-20"
         >
-          <source src="https://your-video-url.mp4" type="video/mp4" />
+          <source src="https://cdn.pixabay.com/video/2022/04/09/113368-697718069_large.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
         <div className="absolute inset-0 bg-gradient-to-b from-white dark:from-black to-transparent opacity-10"></div>
