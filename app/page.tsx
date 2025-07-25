fca3ce0372777db82d23b04489b6c99d51e01745
Aaidt <aadits2004@gmail.com>
2025-07-25 23:29:13 +0530

    added wallet connection button

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 "use client"
 
 import { motion } from "motion/react"
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 const text = "Wallet_ops"
 export default function Home() {
@@ -17,12 +21,22 @@ export default function Home() {
             transition: {
                 staggerChildren: 0.1,
                 delayChildren: 0.3
-            }
+            }       
         }
     }
 
+    const walletVariant = {
+        hidden: { opacity: 0 },
+        visible: { 
+            opacity: 1, 
+            transition: {
+                delayChildren: 1
+            }
+        } 
+    } 
+
     return (
-        <div className="">
+        <div className="flex-col">
             <motion.div variants={containerVariant} initial="hidden" animate="visible" 
                 className="text-5xl font-bold flex justify-center items-center pt-25">
 
@@ -34,6 +48,18 @@ export default function Home() {
                     </motion.span>
                 ))}
             </motion.div>
+
+                <motion.div variants={walletVariant} initial="hidden" animate="visible" className="flex justify-center pt-30">
+                    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+                        <WalletProvider wallets={[]}>
+                            <WalletModalProvider>
+                                <div className="">
+                                    <WalletMultiButton />
+                                </div>
+                            </WalletModalProvider>
+                        </WalletProvider>
+                    </ConnectionProvider>
+                </motion.div>
         </div>
   )
 }
